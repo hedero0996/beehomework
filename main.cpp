aa437b50eade81cfdae4be20c286eab19b9a3153
hedero0996 <aihamw19822@gmail.com>
2025-12-11 19:57:54 +0300

    ss

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 calculator s;
 
-calculator::add(2,3);
+cout <<calculator::add(2,3)<<endl;
 cout<<calculator::mod(4.2,2)<<endl;
 cout<<calculator::factorial(5)<<endl;
 cout<<calculator::sin(0.5)<<endl;
