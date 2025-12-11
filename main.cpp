12472eea505eae94e6226ec29630b6e16ee365c8
hedero0996 <aihamw19822@gmail.com>
2025-12-11 19:54:00 +0300

    change 1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main(){
 calculator s;
 
 calculator::add(2,3);
-cout<<calculator::mod(4.2,2);
-
+cout<<calculator::mod(4.2,2)<<endl;
+cout<<calculator::factorial(5)<<endl;
+cout<<calculator::sin(0.5)<<endl;
+cout<<calculator::cos(0)<<endl;
     return 0;
 }
